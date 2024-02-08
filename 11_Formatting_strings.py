6d52e30c4d13d1cbf5e478dd21dd6a2811af031b
hangman <grigoriy.it@gmail.com>
2024-02-08 14:23:38 +0500

    1.0

---
+++ 11_Formatting_strings.py
@@ -0,0 +1,21 @@
+team1_num = 5
+team2_num = 6
+print('В команде Мастера кода участников: %d !' % team1_num)
+print('Итого сегодня в командах участников: %d и %d !' % (team1_num, team2_num))
+
+score_1 = 40
+score_2 = 42
+print('Команда Волшебники данных решила задач: {} !'.format(score_2))
+
+team1_time = 18015.212
+team2_time = 2153.31451
+print('Волшебники данных решили задачи за {:0.1f} с !'.format(team1_time))
+
+print(f'Команды решили {score_1} и {score_2} задач.')
+
+challenge_result = 'победа команды Волшебники данных!'
+print(f'Результат битвы: {challenge_result}')
+
+tasks_total = 82
+time_avg = 45.2123
+print(f'Сегодня было решенно {tasks_total} задач, в среднем по {time_avg:0.1f} секунды на задачу!')
