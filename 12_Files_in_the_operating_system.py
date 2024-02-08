393b867cc5789fc876ecde4c756601604f5c5953
hangman <grigoriy.it@gmail.com>
2024-02-08 15:02:14 +0500

    1.0

---
+++ 12_Files_in_the_operating_system.py
@@ -0,0 +1,13 @@
+import time, os
+
+directory = '/home/hangman/_Python_projects/'
+
+for path, dirs, files in os.walk(directory):
+    for file in files:
+        filepath = os.path.join(path, file)
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(filepath)
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} '
+              f'байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
