a207773b8f20bc57901513ba13c9adbc8385f1a7
GrigoriyIT <grigoriy.it@gmail.com>
2024-02-15 14:10:35 +0500

    1.1

--- 14_Creating_Exceptions.py
+++ 14_Creating_Exceptions.py
@@ -6,8 +6,24 @@ class ProcessingException(Exception):
     pass
 
 
-try:
-    raise InvalidDataException('Данные не верные')
-except InvalidDataException as exc:
-    print(f'Исключение {exc}')
-    raise ProcessingException('Исключение обработки')
+def water(t):
+    if type(t) is int or type(t) is float:
+        if not 0 < t < 100:
+            raise ProcessingException('Это не вода')
+    else:
+        raise InvalidDataException('Ввели не число')
+    print(f'Температура воды {t} градусов')
+
+
+def input_t(t):
+    try:
+        water(t)
+    except ProcessingException as exc:
+        print(f'{exc}. Температура должна быть от 0 до 100')
+    except InvalidDataException as exc:
+        print(f'{exc}. Температура должна быть числом')
+
+
+input_t(5)
+input_t(500)
+input_t('asd')
