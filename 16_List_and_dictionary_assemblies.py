98fbf326d66574cb599083bbdaea86d0d690c295
hangman <grigoriy.it@gmail.com>
2024-02-26 14:40:15 +0500

    1.01

--- 16_List_and_dictionary_assemblies.py
+++ 16_List_and_dictionary_assemblies.py
@@ -1,4 +1,4 @@
-def mult2(x):
+def squared(x):
     return x ** 2
 
 
@@ -7,5 +7,5 @@ def is_odd(x):
 
 
 my_numbers = [1, 2, 5, 7, 12, 11, 35, 4, 89, 10]
-result = (filter(is_odd, map(mult2, my_numbers)))
+result = (filter(is_odd, map(squared, my_numbers)))
 print(list(result))
