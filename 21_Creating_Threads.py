66ced15bdb4a8495b535e0ea2354acf3e397154a
hangman <grigoriy.it@gmail.com>
2024-03-05 16:24:51 +0500

    1.1

--- 21_Creating_Threads.py
+++ 21_Creating_Threads.py
@@ -9,9 +9,8 @@ def outputs(data):
 
 
 i = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-c = ('a', 'b', 'c', 'd', 'e', 'f', 'j')
+c = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j')
 t = Thread(target=outputs, kwargs=dict(data=i))
 t.start()
 
 outputs(c)
-t.join()
