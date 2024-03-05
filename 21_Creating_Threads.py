c850462e047524ed02ed4c8a64db64d128888ef2
GrigoriyIT <grigoriy.it@gmail.com>
2024-03-06 00:29:17 +0500

    1.2

--- 21_Creating_Threads.py
+++ 21_Creating_Threads.py
@@ -10,7 +10,8 @@ def outputs(data):
 
 i = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 c = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j')
-t = Thread(target=outputs, kwargs=dict(data=i))
-t.start()
+t1 = Thread(target=outputs, kwargs=dict(data=i))
+t1.start()
 
-outputs(c)
+t2 = Thread(target=outputs, kwargs=dict(data=c))
+t2.start()
