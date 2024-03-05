b7b1dba8b4e74a31e534a914d7049c685cd80ae0
hangman <grigoriy.it@gmail.com>
2024-03-05 16:22:01 +0500

    1.0

---
+++ 21_Creating_Threads.py
@@ -0,0 +1,17 @@
+import time
+from threading import Thread
+
+
+def outputs(data):
+    for elem in data:
+        time.sleep(1)
+        print(elem)
+
+
+i = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+c = ('a', 'b', 'c', 'd', 'e', 'f', 'j')
+t = Thread(target=outputs, kwargs=dict(data=i))
+t.start()
+
+outputs(c)
+t.join()
